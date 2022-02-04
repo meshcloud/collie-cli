dbf9d12e145920ea940046acdb0475250a2155a3
Daniel Ritz <dritz@meshcloud.io>
2022-02-04 15:17:08 +0100

    add back/quit in interactiveDate

--- src/commands/interactive/inputInteractiveDate.ts
+++ src/commands/interactive/inputInteractiveDate.ts
@@ -10,11 +10,17 @@ export async function interactiveDate(
   let date = "";
   while (running) {
     date = await Input.prompt(question);
-    if (!dateRegex.test(date.toLowerCase())) {
-      console.log('Please enter a valid date in format YYYY-MM-DD.');
+    if (date.toUpperCase() == "QUIT") {
+      Deno.exit;
+      return "QUIT";
+    } else if(date.toUpperCase() == "BACK") {
+      return "BACK";
+    } else if (!dateRegex.test(date.toLowerCase())) {
+      console.log('Please enter a valid date in format YYYY-MM-DD. Enter "back" to go back or "quit" to quit.');
     } else {
       running = false;
+      return date;
     }
   }
-  return date;
+  
 }
