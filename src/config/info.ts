b266561d94f3f3516d3284ea8a53f9ca418154c1
Johannes Rudolph <jrudolph@meshcloud.io>
2022-04-26 14:20:35 +0200

    refactor: introduce import map and use it for aws, azure, gcp package imports
    Source maps are new in deno, see
    https://deno.land/manual/linking_to_external_code/import_maps.
    This allows us to import dependencies within our code base using absolute
    paths, which is easier to refactor when moving files around

--- src/config/info.ts
+++ src/config/info.ts
@@ -1,4 +1,4 @@
 export const VERSION = "0.9.1";
 export const FLAGS =
-  "--unstable --allow-read --allow-write --allow-env --allow-run";
+  "--unstable --allow-read --allow-write --allow-env --allow-run --import-map=src/import_map.json";
 export const GITHUB_REPO = "meshcloud/collie-cli";
