a9dc24d2739d55d26aec4091e44ef4da6abd0bf7
Johannes Rudolph <jrudolph@meshcloud.io>
2022-04-26 11:11:27 +0200

    test: tweak query statistic tests to avoid false positives

--- src/mesh/query-statistics.test.ts
+++ src/mesh/query-statistics.test.ts
@@ -6,40 +6,40 @@ Deno.test("recording adds up", async () => {
   const sut = new QueryStatistics();
 
   await sut.recordQuery(MeshPlatform.AWS, 0, async () => {
-    await delay(1);
+    await delay(2);
   });
 
   await sut.recordQuery(MeshPlatform.AWS, 0, async () => {
-    await delay(1);
+    await delay(2);
   });
 
-  assert((sut.duration.AWS || 0) >= 2); // not a perfect test, could have false negatives on slow machines
+  assert((sut.duration.AWS || 0) >= 4); // not a perfect test, could have false negatives on slow machines
 });
 
 Deno.test("Writing multiple times with different layer clears values", async () => {
   const sut = new QueryStatistics();
 
   await sut.recordQuery(MeshPlatform.GCP, 0, async () => {
-    await delay(1);
+    await delay(2);
   });
 
   await sut.recordQuery(MeshPlatform.AWS, 1, async () => {
-    await delay(1);
+    await delay(2);
   });
 
   assert(sut.duration.GCP === undefined);
   assert((sut.duration.AWS || 0) >= 1);
 });
 
-Deno.test("Re-entrant calls with different layer only keeps the higherst call", async () => {
+Deno.test("Re-entrant calls with different layers only keeps the highest layer call", async () => {
   const sut = new QueryStatistics();
 
   await sut.recordQuery("cache", 0, async () => {
     await sut.recordQuery(MeshPlatform.AWS, 1, async () => {
-      await delay(1);
+      await delay(2);
     });
     await sut.recordQuery(MeshPlatform.GCP, 1, async () => {
-      await delay(1);
+      await delay(2);
     });
   });
 
