596b7663bef5cdc209b28c5fa268047adce9ae2e
Johannes Rudolph <jrudolph@meshcloud.io>
2022-04-26 11:01:48 +0200

    chore: upgrade to deno 1.21
    some of our transient dependencies depend on std@0.136 which only compiles
    with latest deno version

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs-unstable> { } }:
 
 pkgs.mkShell {
   NIX_SHELL = "collie-cli";
--- src/dev-deps.ts
+++ src/dev-deps.ts
@@ -2,13 +2,11 @@
 // this is a deno best practice https://deno.land/manual@v1.7.4/examples/manage_dependencies
 // for  discussion of the performance implications (and why this doesn't matter much for this _app_) see https://github.com/denoland/deno/issues/6194
 
-export * from "https://deno.land/x/mock@v0.10.0/mod.ts";
-export { stub } from "https://deno.land/x/mock@v0.9.5/stub.ts";
 export {
   assert,
   assertEquals,
   assertMatch,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.98.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.98.0/async/mod.ts";
+} from "https://deno.land/std@0.116.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.116.0/async/mod.ts";
