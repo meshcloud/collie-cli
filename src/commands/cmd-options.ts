48c66c1e2014161ad684933f278beb42fbe3845f
Johannes Rudolph <jrudolph@meshcloud.io>
2022-05-02 21:26:17 +0200

    feat: add desired log levels to main

--- src/commands/cmd-options.ts
+++ src/commands/cmd-options.ts
@@ -4,6 +4,7 @@ import { OutputFormat } from "../presentation/output-format.ts";
 export const OutputFormatType = new EnumType(Object.values(OutputFormat));
 
 export interface CmdGlobalOptions {
+  quiet: boolean;
   debug: boolean;
   verbose: boolean;
   output: OutputFormat;
