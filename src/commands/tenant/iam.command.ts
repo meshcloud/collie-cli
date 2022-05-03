3b71b481d8a7271b05ddbb2780365f1fecebe9f3
Johannes Rudolph <jrudolph@meshcloud.io>
2022-05-03 16:57:32 +0200

    refactor: add support for collie tenant iam --filter.principal option
    this is the old "collie user" command, whose primary output was still
    a tenant list, so modelling it as a tenant filter option makes more sense

--- src/commands/tenant/iam.command.ts
+++ src/commands/tenant/iam.command.ts
@@ -1,11 +1,16 @@
+import { CLICommand } from "../../config/config.model.ts";
 import { Command } from "../../deps.ts";
+import { MeshTenantFilter } from "../../mesh/filter/mest-tenant-filter.ts";
 import { TenantIamPresenterFactory } from "../../presentation/tenant-iam-presenter-factory.ts";
 import { CmdGlobalOptions } from "../cmd-options.ts";
 import { prepareTenantCommand } from "./prepareTenantCommand.ts";
 import { TenantCommandOptions } from "./TenantCommandOptions.ts";
-
+import { PrincipalNameMeshTenantFilter } from "/mesh/filter/principal-name-mesh-tenant-filter.ts";
 interface IamCommandOptions extends CmdGlobalOptions {
   includeAncestors: boolean;
+  filter?: {
+    principal?: string;
+  };
 }
 
 export function registerIamCommand(program: Command) {
@@ -17,6 +22,14 @@ export function registerIamCommand(program: Command) {
     .option(
       "--include-ancestors [includeAncestors:boolean]",
       "Shows the IAM Role Assignments inherited from an ancestor level as well (Azure Management Groups & Root, GCP Folders & Organizations)",
+    )
+    .option(
+      "--filter.principal [principal:string]",
+      "Include only tenants that have the specified principal assigned",
+    )
+    .example(
+      "List all tenants who have the user john.doe@example.com assigned",
+      `${CLICommand} tenant iam <foundation> --filter.principal john.doe@example.com`,
     )
     .action(listIamAction);
 }
@@ -32,8 +45,23 @@ async function listIamAction(
 
   await meshAdapter.attachTenantRoleAssignments(allTenants);
 
+  // TODO: consider adding filtering options to all collie tenant queries
+  const principal = options.filter?.principal;
+
+  const filters: MeshTenantFilter[] = [
+    principal &&
+    (new PrincipalNameMeshTenantFilter(
+      principal,
+      options.includeAncestors,
+    ) as MeshTenantFilter),
+  ].filter((x): x is MeshTenantFilter => !!x);
+
+  const filteredTenants = allTenants.filter((t) =>
+    filters.every((f) => f.filter(t))
+  );
+
   // todo: include query statistics in presenter
   new TenantIamPresenterFactory(tableFactory)
-    .buildPresenter(options.output, options.includeAncestors, allTenants)
+    .buildPresenter(options.output, options.includeAncestors, filteredTenants)
     .present();
 }
