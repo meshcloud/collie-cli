06b04ea65cd2e125c2a3219b9328780650dfd983
Johannes Rudolph <jrudolph@meshcloud.io>
2022-06-02 22:20:57 +0200

    feat: add auto-complete for "foundation deploy --module" option

---
+++ src/commands/foundation/PlatformModuleType.ts
@@ -0,0 +1,43 @@
+import * as fs from "std/fs";
+import * as path from "std/path";
+import { StringType } from "../../deps.ts";
+
+export class PlatformModuleType extends StringType {
+  async complete(): Promise<string[]> {
+    const modules = [];
+
+    for await (
+      const file of fs.expandGlob(
+        "foundations/*/platforms/*/**/terragrunt.hcl",
+        {
+          exclude: ["**/.terragrunt-cache"],
+          globstar: true,
+        },
+      )
+    ) {
+      const id = PlatformModuleType.parseModuleId(file.path);
+      modules.push(id);
+    }
+
+    // I don't know how we could get the foundation name passed to the arg to filter
+    // only to the platforms in the already specified foundation
+    // the foundation arg seems not passed to the completions command, so there's nothing we can do here
+
+    return modules;
+  }
+
+  static parseModuleId(matchedPath: string) {
+    const components = matchedPath.split("/");
+    const platformIndex = components.lastIndexOf("platforms");
+
+    const dropTerragruntHclComponent = -1;
+    const skipPlatformNameComponent = 2;
+
+    const moduleId = components.slice(
+      platformIndex + skipPlatformNameComponent,
+      dropTerragruntHclComponent,
+    );
+
+    return path.join(...moduleId);
+  }
+}
