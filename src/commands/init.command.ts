52382cf1fcd6d6e56f2b819dde7dacc06a64d527
Johannes Rudolph <jrudolph@meshcloud.io>
2022-09-13 12:09:16 +0200

    fix: generate a kit readme in collie init
    closes #168

--- src/commands/init.command.ts
+++ src/commands/init.command.ts
@@ -22,7 +22,13 @@ export function registerInitCommand(program: Command) {
         entries: [
           { name: "README.md", content: readmeMd },
           { name: ".gitignore", content: gitignore },
-          { name: "kit", entries: [{ name: ".gitkeep", content: "" }] },
+          {
+            name: "kit",
+            entries: [
+              { name: ".gitkeep", content: "" },
+              { name: "README.md", content: kitReadmeMd },
+            ],
+          },
           { name: "compliance", entries: [{ name: ".gitkeep", content: "" }] },
           { name: "foundations", entries: [{ name: ".gitkeep", content: "" }] },
         ],
@@ -108,4 +114,27 @@ const gitignore = `# terraform/terragrunt caches
 # collie cache
 **.collie.json
 **.meta.json
+`;
+
+const kitReadmeMd = `# Your Landing Zone Construction Kit
+
+Build your own landing zone construction [kit modules](https://landingzone.meshcloud.io/reference/kit-module.html) in this folder.
+
+## Getting Started
+
+\`\`\`shell
+collie kit new "aws/organization-policies"   # generate a new IaC module skeleton
+collie kit apply "aws/organization-policies" # apply the module to a cloud platform in your foundation
+collie foundation deploy "my-foundation"     # deploy the module to your cloud foundation
+\`\`\`
+
+You can build your own kit modules from scratch or fork existing modules from the [kit module library](https://github.com/meshcloud/landing-zone-construction-kit/tree/main/kit)
+by copying them here.
+
+> Beware that kit modules identifiers must always have a cloud provider prefix like \`aws/my-module\` in their name.
+
+## Documentation
+
+This page is also included in your cloud foundation's documentation generated with \`collie foundation docs\`. Use this
+document to describe anything that engineers working on the kit need to know.
 `;
