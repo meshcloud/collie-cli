25dcbe0dae5759d33aefcc86f78f7ba332dd66a7
Johannes Rudolph <jrudolph@meshcloud.io>
2022-09-29 10:37:35 +0200

    fix: relax git version parsing for custom git builds
    many distros ship custom git builds that return distro-sepcific
    build info in "git --version" output

--- src/api/git/GitCliDetector.ts
+++ src/api/git/GitCliDetector.ts
@@ -7,16 +7,18 @@ export class GitCliDetector extends CliDetector {
     super("git", runner);
   }
 
-  protected parseVersion(versionCmdOutput: string): string {
-    const components = versionCmdOutput.split(" ");
+  public parseVersion(versionCmdOutput: string): string {
+    // some git version strings have additional build info in them, so we only extract what looks like a version number
+    const versionRegex = /\d+.\d+.\d+/g;
 
-    const version = components[0].substring("git version ".length);
+    const matches = versionCmdOutput.match(versionRegex);
 
-    return version;
+    return (matches && matches[0]) || "";
   }
 
   protected isSupportedVersion(version: string): boolean {
     // a simple lexicographic comparison is sufficient for our needs
+
     return version > "2.0.0";
   }
 }
