c711422f7dddd40f72014723a2d4aeaa991a1336
Johannes Rudolph <jrudolph@meshcloud.io>
2022-09-29 09:47:14 +0200

    chore: fix deno task run on windows
    correctly handle building source map path on windows

--- flags.ts
+++ flags.ts
@@ -1,5 +1,9 @@
 import { FLAGS } from "./src/info.ts";
+import { isWindows } from "./src/os.ts";
 
-const __dirname = new URL(".", import.meta.url).pathname;
+let __dirname = new URL(".", import.meta.url).pathname;
+if (isWindows) {
+  __dirname = __dirname.substring(1); // on windows we have to strip the path so it looks like C:/... instead of /C:/...
+}
 
 console.log(FLAGS + ` --import-map=${__dirname}src/import_map.json`);
