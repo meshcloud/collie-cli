8310320f8ded19bb65d3fd52055c0654f96b6f90
Fabian Muscariello <fmuscariello@meshcloud.io>
2022-10-19 18:25:22 +0200

    chore: remove pax_global_header file

--- src/commands/kit/kit-download.ts
+++ src/commands/kit/kit-download.ts
@@ -1,4 +1,5 @@
 import { readerFromStreamReader, copy} from "std/streams/conversion";
+import * as path from "std/path";
 
 import { tgz } from "x/tar";
 import { MeshError } from "../../errors.ts";
@@ -9,6 +10,18 @@ export async function kitDownload(modulePath: string, url: string) {
   // like caf-terraform-landingzones-57d67d2640ea8541e639d60fc70de5a3409c8876
   await tgz.uncompress(tmpFilepath, modulePath);
   await Deno.remove(tmpFilepath);
+  // tar archives created from git repositories often include a pax_global_header file which
+  // includes metadata. It includes no relevant info for the user, so we remove it.
+  const headerFile = path.join(modulePath, 'pax_global_header');
+  try {
+    await Deno.remove(headerFile);
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      // Nothing to remove if the file does not exist.
+    } else {
+      throw e;
+    }
+  }
 }
 
 async function downloadToTemporaryFile(url: string): Promise<string> {
