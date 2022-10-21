ba1a1335eec6698a65f5ae3e492e902040c63aeb
Johannes Schwandke <jschwandke@meshcloud.io>
2022-10-21 13:28:18 +0200

    feat: first version of an autodeploy flag in kit representation

--- src/commands/kit/bundles/azure-caf-es.ts
+++ src/commands/kit/bundles/azure-caf-es.ts
@@ -11,13 +11,15 @@ export class AzureKitBundle extends KitBundle {
         "https://github.com/meshcloud/landing-zone-construction-kit/archive/5ce27391f94ab2c9b6b93cfb554c43ba0628b97d.tar.gz",
         "/kit/azure/bootstrap-es",
         [],
-        undefined)
+        undefined,
+        true)
       ],
       ["base", new KitRepresentation(
         "https://github.com/Azure/terraform-azurerm-caf-enterprise-scale/archive/refs/tags/v2.4.1.tar.gz",
          undefined,
          [],
-         new KitMetadata("Azure CAF Enterprise Scale", "todo description goes here"))
+         new KitMetadata("Azure CAF Enterprise Scale", "todo description goes here"),
+         false)
         ]
     ]);
   }
