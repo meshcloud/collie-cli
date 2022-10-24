f0cd5d4af17a60e6ae7518cbed713170c09436fc
Fabian Muscariello <fmuscariello@meshcloud.io>
2022-10-24 16:10:29 +0200

    chore: call betweenDoubleDeployments if required

--- src/commands/kit/bundle.command.ts
+++ src/commands/kit/bundle.command.ts
@@ -100,6 +100,8 @@ export function registerBundledKitCmd(program: TopLevelCommand) {
         // TODO deployment is commented for now, some TODOs are open before this can be used.
         // await deployFoundation(collie, foundationRepo, mode, opts, logger);
         if (kitRepr.deployment?.needsDoubleDeploy) {
+          // TODO instead of passing an empty map, pass the actual arguments.
+          // kitRepr.deployment.betweenDoubleDeployments!(platformPath, new Map());
           // await deployFoundation(collie, foundationRepo, kitRepr.deployment.secondDeploymentArgs, opts, logger);
         }
       });
