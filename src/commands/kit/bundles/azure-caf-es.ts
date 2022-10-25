ad3f43655db7ba864bd18a517b0975435a8b165c
Johannes Schwandke <jschwandke@meshcloud.io>
2022-10-25 16:08:01 +0200

    feat: parametrization for base module

--- src/commands/kit/bundles/azure-caf-es.ts
+++ src/commands/kit/bundles/azure-caf-es.ts
@@ -7,8 +7,13 @@ export class AzureKitBundle extends KitBundle {
     super(identifier, displayName);
   }
 
+  // Define Parameter names globally for easier change:
+  const PARAM_ROOT_ID = "Root Id";
+  const PARAM_ROOT_NAME = "Root Name";
+  const PARAM_DEFAULT_LOCATION = "Default Location";
+
   kitsAndSources(): Map<string, KitRepresentation> {
-    const bootstrapParams: InputParameter[] = [
+    const bootstrapKitParams: InputParameter[] = [
       {
         description: "Platform Engineer Email",
         // validating e-mails by regex is generally considered a futile attempt, so we accept everything.
@@ -30,14 +35,35 @@ export class AzureKitBundle extends KitBundle {
         validationFailureMessage: 'Please enter a valid azure availability zone.',
       },
     ];
-    const baseParams: InputParameter[] = [];
+
+    // TODO improve on those
+    const baseKitParams: InputParameter[] = [
+      {
+        description: this.PARAM_ROOT_ID,
+        validationRegex: /.*/,
+        hint: undefined,
+        validationFailureMessage: '',
+      },
+      {
+        description: this.PARAM_ROOT_NAME,
+        validationRegex: /.*/,
+        hint: undefined,
+        validationFailureMessage: '',
+      },
+      {
+        description: this.PARAM_DEFAULT_LOCATION,
+        validationRegex: /.*/,
+        hint: undefined,
+        validationFailureMessage: '',
+      },
+    ];
 
     return new Map<string, KitRepresentation>([
 
       ["bootstrap", new KitRepresentation(
         "https://github.com/meshcloud/landing-zone-construction-kit/archive/014e8d3d9432b2d18af5cde1ea28bfbab50c7832.tar.gz",
         "/kit/azure/bootstrap-es",
-        bootstrapParams,
+        bootstrapKitParams,
         undefined,
         new KitDeployRepresentation(0, true, this.betweenDeployments, { raw: ["apply"] }))
       ],
@@ -45,7 +71,7 @@ export class AzureKitBundle extends KitBundle {
       ["base", new KitRepresentation(
         "https://github.com/Azure/terraform-azurerm-caf-enterprise-scale/archive/refs/tags/v2.4.1.tar.gz",
          undefined,
-         baseParams,
+         baseKitParams,
          new KitMetadata("Azure CAF Enterprise Scale", "todo description goes here"),
          undefined)
       ]
@@ -131,8 +157,66 @@ export class AzureKitBundle extends KitBundle {
     Deno.writeTextFileSync(platformHCL, text);
   }
 
-  afterApplyBase(_platformModuleDir: string, _parametrization: Map<string,string>): void {
-    //TODO
+  afterApplyBase(platformModuleDir: string, parametrization: Map<string,string>): void {
+    const baseOutputTF = path.join(platformModuleDir, "base", "output.tf");
+    const moduleHCL = path.join(platformModuleDir, "module.hcl");
+    const baseTerragrunt = path.join(platformModuleDir, "base", "terragrunt.hcl");
+
+    const outputBlockLS = '  value = {\n' +
+      	                  '    management = azurerm_log_analytics_linked_service.management\n' +
+                          '  }\n' +
+                          '  description = "Returns the configuration data for all Log Analytics linked services created by this module."';
+
+    const outputReplacementBlockLS = '  value = {\n' +
+      	                  '    management = azurerm_log_analytics_linked_service.management\n' +
+                          '    Sensitive = true\n'
+                          '  }\n' +
+                          '  description = "Returns the configuration data for all Log Analytics linked services created by this module."';
+
+    const oldProvidersBlock = '  provider "todo" {
+                              '    # tip: you can access collie configuration from the local above, e.g. "${local.platform.azure.aadTenantId}"\n' +
+      	                      '    # tip: you can access bootstrap module output like secrets from the dependency above, e.g. "${dependency.bootstrap.outputs.client_secret}"\n' +
+                              '  }';
+
+    const newProvidersBlock = '\n' +
+                              'provider "azurerm" {\n' +
+                              '  features {}\n' +
+                              '  alias                      = "connectivity"\n' +
+                              '  subscription_id            = "${local.platform.azure.subscriptionId}"\n' +
+                              '}\n' +
+                              'provider "azurerm" {\n' +
+                              '  features {}\n' +
+                              '  alias                      = "management"\n' +
+                              '  subscription_id            = "${local.platform.azure.subscriptionId}"\n' +
+                              '}\n';
+
+    const baseInputToken = '# todo: specify inputs to terraform module';
+
+    const baseInputVariables = '\n' +
+                               'root_parent_id = "${include.platform.locals.platform.azure.aadTenantId}"\n' +
+                               `root_id        = "${parametrization.get(this.PARAM_ROOT_ID)}"\n` +
+                               `root_name      = "${parametrization.get(this.PARAM_ROOT_NAME)}"\n` +
+                               `default_location = "${parametrization.get(this.PARAM_DEFAULT_LOCATION)}"\n` +
+                               'deploy_corp_landing_zones = true\n' +
+                               'deploy_online_landing_zones = true\n' +
+                               '# Management resources\n' +
+                               'deploy_management_resources = true\n' +
+                               'subscription_id_management  = "${include.platform.locals.platform.azure.subscriptionId}" # Subscription created manually as a prerequisite\n';
+
+    // update base/output.tf
+    let text = Deno.readTextFileSync(baseOutputTF);
+    text = text.replace(outputBlockLS, outputReplacementBlockLS);
+    Deno.writeTextFileSync(baseOutputTF, text);
+
+    // update module.hcl
+    text = Deno.readTextFileSync(moduleHCL);
+    text = text.replace(oldProvidersBlock, newProvidersBlock);
+    Deno.writeTextFileSync(moduleHCL, text);
+
+    // update base/terragrunt.hcl
+    text = Deno.readTextFileSync(baseTerragrunt);
+    text = text.replace(baseInputToken, baseInputVariables);
+    Deno.writeTextFileSync(baseTerragrunt, text);
   }
 
   afterApply(platformModuleDir: string, parametrization: Map<string,string>): void {
