4ce04aafc996747d8fcf8ba2b58dc70baedd5fe1
Johannes Schwandke <jschwandke@meshcloud.io>
2022-12-14 15:53:44 +0100

    fix: fix windows paths for deeper nested paths as well

--- src/info.ts
+++ src/info.ts
@@ -1,5 +1,5 @@
 // see https://stackoverflow.com/questions/61829367/node-js-dirname-filename-equivalent-in-deno
-export const VERSION = "0.15.1";
+export const VERSION = "0.15.2";
 
 /**
  * The flags we want collie to be invoked with.
