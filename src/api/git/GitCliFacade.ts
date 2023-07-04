e265a7c29ce3fb5b82a83d4f42d0e893c4db83af
Johannes Rudolph <jrudolph@meshcloud.io>
2023-07-04 17:41:07 +0200

    feat: first rough implementation of a kit import command

--- src/api/git/GitCliFacade.ts
+++ src/api/git/GitCliFacade.ts
@@ -4,9 +4,20 @@ import { ProcessResult } from "../../process/ProcessRunnerResult.ts";
 export class GitCliFacade {
   constructor(private readonly processRunner: IProcessRunner<ProcessResult>) {}
 
+  // todo: should we have a repoDir parameter like everywhere else? cwd is implicit here
   async init() {
     // note: rerunning git init is safe
     // https://stackoverflow.com/questions/5149694/does-running-git-init-twice-initialize-a-repository-or-reinitialize-an-existing
     await this.processRunner.run(["git", "init"]);
+  }
+
+  async clone(destDir: string, repoUrl: string) {
+    // note: rerunning git init is safe
+    // https://stackoverflow.com/questions/5149694/does-running-git-init-twice-initialize-a-repository-or-reinitialize-an-existing
+    await this.processRunner.run(["git", "clone", repoUrl, destDir]);
+  }
+
+  async pull(repoDir: string) {
+    await this.processRunner.run(["git", "pull"], { cwd: repoDir });
   }
 }
