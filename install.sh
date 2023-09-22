38e953d6a0a3bd0ce0f4bacb6fd5c61eca315d8d
Johannes Rudolph <jrudolph@meshcloud.io>
2023-09-22 11:54:07 +0200

    test: add e2e tests for windows and linux
    - add COLLIE_VERSION env var support to install scripts
    - fix a typo in collie compliance tree warning

--- install.sh
+++ install.sh
@@ -21,15 +21,22 @@ for i in $REQ_COMMANDS
 do
   command -v "$i" >/dev/null && continue || { echo "$i command not found. You need to install if before running this script"; exit 1; }
 done
+
+COLLIE_VERSION=${COLLIE_VERSION:-}
+release_url="https://api.github.com/repos/meshcloud/collie-cli/releases/latest"
+if [ ! -z "${COLLIE_VERSION}" ]; then
+  release_url="https://api.github.com/repos/meshcloud/collie-cli/releases/tags/${COLLIE_VERSION}"
+fi
+
 # Download artifacts based on CPU architecture and OS type
 case "$(uname -s)" in
    Darwin)
-      url=$(curl -s https://api.github.com/repos/meshcloud/collie-cli/releases/latest | grep "browser_download_url.*$arch.*apple" | cut -d : -f 2,3 | tr -d \" | tr -d \ )
+      artifact_url=$(curl -s "$release_url" | grep "browser_download_url.*$arch.*apple" | cut -d : -f 2,3 | tr -d \" | tr -d \ )
       name="collie-$arch-apple-darwin"    
      ;;
    Linux)
     echo "detected Linux"
-     url=$(curl -s https://api.github.com/repos/meshcloud/collie-cli/releases/latest | grep "browser_download_url.*linux" | cut -d : -f 2,3 | tr -d \" | tr -d \ )
+     artifact_url=$(curl -s "$release_url" | grep "browser_download_url.*linux" | cut -d : -f 2,3 | tr -d \" | tr -d \ )
      name="collie-x86_64-unknown-linux-gnu"
      ;;
    CYGWIN*|MINGW32*|MSYS*|MINGW*)
@@ -42,8 +49,8 @@ case "$(uname -s)" in
      ;;
 esac
 
-echo "Downloading the artifact... (${url})"
-curl "${url}" -L -o collie.tar.gz --silent
+echo "Downloading the artifact... (${artifact_url})"
+curl "${artifact_url}" -L -o collie.tar.gz --silent
 # untar and movin the artifact
 echo "Unpacking the artifact and copying the binary..."
 tar -zxf collie.tar.gz -C /usr/local/bin
---
+++ test/e2e.ps1
@@ -0,0 +1,61 @@
+$tmpdir=New-TemporaryFile | %{ rm $_; mkdir $_ }
+echo "Running test in $tmpdir - please remember to cleanup"
+
+cd $tmpdir
+
+# don't ask..., if you must - https://stackoverflow.com/questions/5596982/using-powershell-to-write-a-file-in-utf-8-without-the-bom#comment79966549_5596984
+[System.Environment]::CurrentDirectory = (Get-Location).Path
+
+collie init .
+
+echo "Testing foundation features"
+collie foundation new f
+
+# fake a platform config file
+
+# equivalent of mkdir -p
+md foundations/f/platforms/az/ -ea 0
+
+# powershell needs some convicing to write files at UTF8 without BOM... this is the only thing that works
+$platform_md=@"
+---
+id: az
+name: Likvid Bank Azure
+azure:
+    aadTenantId: 1234-1234
+    subscriptionId: 1234-1234
+cli: 
+    az: {}
+---
+# Likvid Bank Azure
+Test test
+"@
+[System.IO.File]::WriteAllLines("foundations/f/platforms/az/README.md", $platform_md)
+
+
+echo "Testing kit features"
+collie kit import foundation/docs
+collie kit import azure/bootstrap
+collie kit apply azure/bootstrap --foundation f --platform az
+
+collie kit new azure/dummy dummy
+
+$variables_tf=@"
+variable "my_input_var" {}
+"@
+[IO.File]::WriteAllLines("kit/azure/dummy/variables.tf", $variables_tf)
+
+collie kit apply azure/dummy --foundation f --platform az
+
+collie foundation deploy --auto-approve
+
+echo "Testing compliance features"
+collie compliance import cfmm
+
+echo "Testing outputs"
+collie foundation tree
+collie kit tree
+collie compliance tree
+
+echo "Testing documentation"
+collie foundation docs --build
