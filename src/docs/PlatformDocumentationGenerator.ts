ff2605466483f77a9c2067bc412a550a38cb3293
Johannes Rudolph <jrudolph@meshcloud.io>
2023-11-23 19:21:45 +0100

    style: fix lint issue

--- src/docs/PlatformDocumentationGenerator.ts
+++ src/docs/PlatformDocumentationGenerator.ts
@@ -1,6 +1,5 @@
 import * as fs from "std/fs";
 import * as path from "std/path";
-import { pooledMap } from "std/async";
 import { TerragruntCliFacade } from "../api/terragrunt/TerragruntCliFacade.ts";
 import { Logger } from "../cli/Logger.ts";
 import { ProgressReporter } from "../cli/ProgressReporter.ts";
