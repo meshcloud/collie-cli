189c21b0ee23ca4754da7cac803a967ff4c269ba
Gregor Zavodnik <gregorzavodnik26@gmail.com>
2024-07-23 11:33:58 +0200

    Get rid of AWS Refs, useless constants and adjusting schema.json

--- src/api/custom/CustomCliFacade.ts
+++ src/api/custom/CustomCliFacade.ts
@@ -1,27 +1,3 @@
-import {
-   Account,
-   AccountsResponse,
-   AssumedRoleResponse,
-   CallerIdentity,
-   CostResponse,
-   Credentials,
-   Group,
-   GroupResponse,
-   OrganizationalUnit,
-   OrganizationalUnitsResponse,
-   Policy,
-   PolicyResponse,
-   RegionsResponse,
-   Root,
-   RootResponse,
-   Tag,
-   TagsResponse,
-   User,
-   UserResponse,
- } from "/api/aws/Model.ts";
-//  import { moment } from "x/deno_moment";
-//  import { sleep } from "/promises.ts";
- import { parseJsonWithLog } from "/json.ts";
  import { ProcessResultWithOutput } from "../../process/ProcessRunnerResult.ts";
  import { IProcessRunner } from "../../process/IProcessRunner.ts";
 
@@ -30,16 +6,5 @@ import {
      private readonly processRunner: IProcessRunner<ProcessResultWithOutput>,
    ) {}
 
-   /**
-    * @param credential Assumed credentials.
-    */
-   async getCallerIdentity(
-     credential?: Credentials,
-     profile?: string,
-   ): Promise<CallerIdentity> {
-     const env = {
-       ...this.credsToEnv(credential),
-     };
-   }
  }
  
