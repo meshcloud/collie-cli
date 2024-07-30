cc563527f991d554dedcdf99c6c229faffcdcc39
Gregor Zavodnik <gregorzavodnik26@gmail.com>
2024-07-30 09:49:37 +0200

    Deno formatted

--- src/api/custom/CustomPlatformSetup.ts
+++ src/api/custom/CustomPlatformSetup.ts
@@ -4,40 +4,39 @@ import { Dir } from "../../cli/DirectoryGenerator.ts";
 import { PlatformSetup } from "../PlatformSetup.ts";
 
 export class CustomPlatformSetup extends PlatformSetup<PlatformConfigCustom> {
-   constructor() {
-      super();
-   }
+  constructor() {
+    super();
+  }
 
-   async promptInteractively(): Promise<PlatformConfigCustom> {
+  async promptInteractively(): Promise<PlatformConfigCustom> {
+    const { id, name, type } = await this.promptPlatformName();
 
-   const { id, name, type } = await this.promptPlatformName();
-
-   return {
+    return {
       id,
       name,
       type,
-   };
-}
+    };
+  }
 
-preparePlatformDir(config: PlatformConfigCustom): Dir {
-   return {
+  preparePlatformDir(config: PlatformConfigCustom): Dir {
+    return {
       name: config.id,
       entries: [
-         { name: "README.md", content: this.generateCustomReadmeMd(config) },
+        { name: "README.md", content: this.generateCustomReadmeMd(config) },
       ],
-   };
-}
+    };
+  }
 
-private generateCustomReadmeMd(config: PlatformConfigCustom): string {
-   const frontmatter = config;
-   const md = `
+  private generateCustomReadmeMd(config: PlatformConfigCustom): string {
+    const frontmatter = config;
+    const md = `
 # ${config.name}
 
 This is a custom platform.
 `;
 
-   const doc = new MarkdownDocument(frontmatter, md);
+    const doc = new MarkdownDocument(frontmatter, md);
 
-   return doc.format();
-   }
+    return doc.format();
+  }
 }
